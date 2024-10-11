c802d4c90ee7d280165b3df32579f9f48e379cb7
d4rkning <chris.schellekens12@gmail.com>
2024-10-11 11:27:51 +0200

    added if conditino into master_launch

--- dabom_ws/launch/master_launch.py
+++ dabom_ws/launch/master_launch.py
@@ -1,12 +1,16 @@
 from launch import LaunchDescription
+import yaml
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition
 import os
 from typing import Optional
 
+
 def generate_launch_file_path(package_name: str, launch_file_name: Optional[str] = None) -> str:
+
     package_dir = get_package_share_directory(package_name)
     if launch_file_name is None:
         launch_file_path =  os.path.join(
@@ -28,6 +32,13 @@ def generate_launch_file_path(package_name: str, launch_file_name: Optional[str]
 
 
 def generate_launch_description():
+    config_file_path = os.path.join(
+        os.getenv('HOME'), 'documents', 'dabom', 'dabom_ws', 'src', 'dabom_bringup', 'config', 'launch_config.yaml'
+    ) 
+
+    with open(config_file_path, 'r') as file:
+        config = yaml.safe_load(file)
+
     packages_launch_files = [
         ["unique_joint_state_publisher"],
         ["odom"],
@@ -49,7 +60,21 @@ def generate_launch_description():
             launch_description = generate_launch_file_path(package[0])
         else:
             print("[ERROR] check package_launch_files list in master launch element containts to many elements should be 1 or 2")
-        launch_descriptions.append(IncludeLaunchDescription(PythonLaunchDescriptionSource(launch_description)))
+        
+        if package[0] not in config:
+            print(f"[ERROR] package '{package[0]}' not in yaml-file '{config_file_path}'")
+            print("QUITTING")
+            quit()
+        else:
+            print("Correctly read yaml file")
+            should_be_run = str(config[package[0]]).lower()
+            print(should_be_run)
+
+        launch_descriptions.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(launch_description),
+                condition=IfCondition(should_be_run)
+                ))
         
 
 
