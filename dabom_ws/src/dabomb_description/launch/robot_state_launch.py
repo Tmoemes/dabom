2003dd92adc26f116d9443c14b9199e6787bfa99
Kargchristopher <christopher@karg.ca>
2024-10-12 22:08:13 +0200

    Created URDF and Robot joint state

---
+++ dabom_ws/src/dabomb_description/launch/robot_state_launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('dabomb_description'),
+        'urdf',
+        'dabomb.xacro')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', urdf_file])}]
+        )
+    ])
