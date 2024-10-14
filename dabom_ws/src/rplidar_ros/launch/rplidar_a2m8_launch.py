ba27f66e8491772cc21bd32bf8883966580d3be8
Kargchristopher <christopher@karg.ca>
2024-10-14 12:26:30 +0200

    lidar launch from integration

--- dabom_ws/src/rplidar_ros/launch/rplidar_a2m8_launch.py
+++ dabom_ws/src/rplidar_ros/launch/rplidar_a2m8_launch.py
@@ -1,31 +1,70 @@
 #!/usr/bin/env python3
 
 import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Point to the YAML config file
-    config_file_path = os.path.join(
-        get_package_share_directory('rplidar_ros'),
-        'config',
-        'rplidar_a2m8.yaml'
-    )
-
-    # Ensure the YAML file exists
-    if not os.path.exists(config_file_path):
-        raise FileNotFoundError(f"Could not find YAML file: {config_file_path}")
-
+    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
+    frame_id = LaunchConfiguration('frame_id', default='laser')
+    inverted = LaunchConfiguration('inverted', default='true')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    scan_mode = LaunchConfiguration('scan_mode', default='Sensitivity')
+    
     return LaunchDescription([
 
-        # Define the RPLIDAR node and load parameters from YAML
+        DeclareLaunchArgument(
+            'channel_type',
+            default_value=channel_type,
+            description='Specifying channel type of lidar'),
+        
+        DeclareLaunchArgument(
+            'serial_port',
+            default_value=serial_port,
+            description='Specifying usb port to connected lidar'),
+
+        DeclareLaunchArgument(
+            'serial_baudrate',
+            default_value=serial_baudrate,
+            description='Specifying usb port baudrate to connected lidar'),
+        
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value=frame_id,
+            description='Specifying frame_id of lidar'),
+
+        DeclareLaunchArgument(
+            'inverted',
+            default_value=inverted,
+            description='Specifying whether or not to invert scan data'),
+
+        DeclareLaunchArgument(
+            'angle_compensate',
+            default_value=angle_compensate,
+            description='Specifying whether or not to enable angle_compensate of scan data'),
+        DeclareLaunchArgument(
+            'scan_mode',
+            default_value=scan_mode,
+            description='Specifying scan mode of lidar'),
+
+
         Node(
-            package='rplidar_ros',
-            executable='rplidar_node',
-            name='rplidar_node',
-            parameters=[config_file_path],  # Load the YAML parameters
-            output='screen',
-        ),
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node',
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port, 
+                         'serial_baudrate': serial_baudrate, 
+                         'frame_id': frame_id,
+                         'inverted': inverted, 
+                         'angle_compensate': angle_compensate}],
+            output='screen'),
     ])
