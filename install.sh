25493f00f17cfbd64eb39f49899e2df87afac86a
Abdulhadi Alwattar <167977112+MrTOXIC165@users.noreply.github.com>
2024-10-18 14:46:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ sudo apt install ros-humble-tf2-ros
 sudo apt install python3-rclpy
 sudo apt install ros-humble-rviz2
 sudo apt install ros-humble-robot-state-publisher
+sudo apt install python3-colcon-common-extensions
+
 
 pip3 install numpy
 pip3 install pyserial
