efd642e9b6af0a4c67785ce4fde8a83942ace578
chriss550 <368537@student.fontys.nl>
2024-10-18 11:53:56 +0200

    removed joy teleop

--- dabom_ws/src/dabom_bringup/launch/computer_bringup.py
+++ dabom_ws/src/dabom_bringup/launch/computer_bringup.py
@@ -18,17 +18,12 @@ def generate_launch_description():
     # Load the launch config YAML file
     with open(launch_config_path, 'r') as f:
         config = yaml.safe_load(f)
-        use_joy = config['launch_config'].get('use_joy', False)
         use_rviz = config['launch_config'].get('use_rviz', True)
         use_slam = config['launch_config'].get('use_slam', False)
         use_nav2 = config['launch_config'].get('use_nav2', False)
         use_robot_state = config['launch_config'].get('use_robot_state', True)
         use_xbox = config['launch_config'].get('use_xbox', True)
 
-    # Paths to individual launch files
-    joy_teleop_launch = os.path.join(
-        get_package_share_directory('dabom_joy'), 'launch', 'joy_teleop_launch.py'
-    )
     slam_params_path = os.path.join(
         get_package_share_directory('dabom_bringup'), 'config', 'mapper_params_online_async.yaml'
     )
@@ -63,13 +58,6 @@ def generate_launch_description():
         ld.add_action(LogInfo(msg="Launching robot state publisher..."))
         ld.add_action(IncludeLaunchDescription(
             PythonLaunchDescriptionSource(robot_state_launch)
-        ))
-
-    # Conditionally launch the joystick control
-    if use_joy:
-        ld.add_action(LogInfo(msg="Launching joystick control..."))
-        ld.add_action(IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(joy_teleop_launch)
         ))
 
     # Conditionally launch RViz
