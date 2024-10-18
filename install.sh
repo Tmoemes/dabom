35d9f7ce8347bc030f3415b6dd6bc015f4326f5f
MrTOXIC165 <hadywattar123456789@gmail.com>
2024-10-18 11:56:30 +0200

    rename install.sh and update it

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt update
+
+sudo apt install python3-pip
+
+
+sudo apt install ros-humble-nav2-*
+sudo apt install ros-humble-slam-toolbox
+sudo apt install ros-humble-tf2-ros
+sudo apt install python3-rclpy
+sudo apt install ros-humble-rviz2
+sudo apt install ros-humble-robot-state-publisher
+
+pip3 install numpy
+pip3 install pyserial
+pip3 install pyyaml
+
+
