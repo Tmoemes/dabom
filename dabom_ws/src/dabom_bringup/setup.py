37a3edb053a29ab0b3ddb5cd79d0cdd6f164b732
Kargchristopher <christopher@karg.ca>
2024-10-19 07:59:25 +0200

    added launch .py manualy

--- dabom_ws/src/dabom_bringup/setup.py
+++ dabom_ws/src/dabom_bringup/setup.py
@@ -12,7 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'), [
+            'launch/computer_bringup.py',
+            'launch/rpi_bringup.py',
+            'launch/simulation.py'
+        ]),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
     ],
