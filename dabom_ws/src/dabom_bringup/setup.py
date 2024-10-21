63c08ba88fae3f978941a05c6abacf819318fdc1
Kargchristopher <christopher@karg.ca>
2024-10-21 14:52:50 +0200

    rollback

--- dabom_ws/src/dabom_bringup/setup.py
+++ dabom_ws/src/dabom_bringup/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from glob import glob
 import os
 
@@ -7,30 +7,24 @@ package_name = 'dabom_bringup'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[],  # No Python packages to install
+    packages=find_packages(include=[package_name]),  # Automatically find the package
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        # Include all config files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),  # Include all Python launch files
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        # Include any RViz configuration files
         (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
-        # Include any maps (if you have a maps directory)
-        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Christopher',
+    maintainer='christopher',
     maintainer_email='christopher@karg.ca',
     description='Master launch package for bringing up the entire robot system',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # No console scripts in this package
         ],
     },
 )
